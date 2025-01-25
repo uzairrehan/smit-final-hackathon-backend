4aed4c69b7bfca36d2d392ced611313997209288
Muhammad Uzair Rehan <iamsyeduzairshah@gmail.com>
2025-01-26 00:39:52 +0500

    usermodal

--- models/user.model.js
+++ models/user.model.js
@@ -1,26 +1,26 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    unique: true,
+  cnic: { 
+    type: String, 
+    unique: true, 
     required: true,
+    validate: {
+      validator: function(value) {
+        // Ensure the CNIC is exactly 13 characters
+        return value.length === 13;
+      },
+      message: 'CNIC must be exactly 13 characters long.'
+    }
   },
-  username: {
-    type: String,
-    unique: true,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: [5, "Password must be at least 5 characters"],
-  },
-  verified: {
-    type: Boolean,
-    default: false,
-  },
+  email: { type: String, unique: true, required: true },
+  name: { type: String, required: true },
+  password: { type: String, required: true },
+  loanRequests: [{ type: mongoose.Schema.Types.ObjectId, ref: 'LoanRequest' }],
+  isPasswordChanged: { type: Boolean, default: false },
 });
 
-const User = mongoose.model("users", userSchema);
-export default User;
+const UserModal = mongoose.model('User', userSchema);
+
+
+export default UserModal
