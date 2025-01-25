23fe5c204e88fd601975a3ef1059bce3e416a6f0
Muhammad Uzair Rehan <iamsyeduzairshah@gmail.com>
2025-01-25 11:34:15 +0500

    env and cors added

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
 import express from "express";
 import mongoConnection from "./config/mongo.connection.js";
 import authroutes from "./routes/auth.routes.js";
-import cors from "cors"
+import cors from "cors";
 import sendResponse from "./helpers/send.response.js";
 const app = express();
 mongoConnection();
 
-const PORT = process.env.PORT
+const allowedOrigins = process.env.CORS_ALLOWED.split(",");
 
+app.use(
+  cors({
+    origin: allowedOrigins,
+  })
+);
 
-app.use(cors("*"))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use("/authentication" ,authroutes)
+app.use("/auth", authroutes);
 
-
-app.get("/",(req,res)=>{
-  res.send("Hello Uzair")
-})
-
-
-
-app.use((req, res) => {
-     sendResponse(res, 400, {}, true,  "Page not found");
+app.get("/", (req, res) => {
+  res.send("Hello Uzair");
 });
 
-app.listen(PORT, () => {
-  console.log(`App is running on localhost:${PORT}`);
-}); 
+app.use((req, res) => {
+  sendResponse(res, 400, {}, true, "Page not found");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`App is running on localhost:${process.env.PORT}`);
+});
